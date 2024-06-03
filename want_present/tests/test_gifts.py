7199fab5adf0fb804a0aae85aa34879e4ddf8677
Andrey Khlimanenkov <hlimanenkov@gmail.com>
2024-06-03 22:10:34 +0300

    тест на изменение подарка

--- want_present/tests/test_gifts.py
+++ want_present/tests/test_gifts.py
@@ -8,8 +8,26 @@ from rest_framework import status
         (pytest.lazy_fixture('admin_client'), status.HTTP_204_NO_CONTENT),
         (pytest.lazy_fixture('author_client'), status.HTTP_204_NO_CONTENT),
         (pytest.lazy_fixture('not_author_client'), status.HTTP_403_FORBIDDEN),
+        (pytest.lazy_fixture('client'), status.HTTP_401_UNAUTHORIZED),
     ]
 )
 def test_del_gift_another_user(client_user, answer, url_gift):
     response = client_user.delete(url_gift)
+    assert response.status_code == answer
+
+
+@pytest.mark.parametrize(
+    'client_user, answer',
+    [
+        (pytest.lazy_fixture('admin_client'), status.HTTP_200_OK),
+        (pytest.lazy_fixture('author_client'), status.HTTP_200_OK),
+        (pytest.lazy_fixture('not_author_client'), status.HTTP_403_FORBIDDEN),
+        (pytest.lazy_fixture('client'), status.HTTP_401_UNAUTHORIZED),
+    ]
+)
+def test_change_short_name_gift(gift, client_user, url_gift, answer):
+    response = client_user.patch(
+        url_gift,
+        {'short_name': 'Измененное название подарка'},
+    )
     assert response.status_code == answer
